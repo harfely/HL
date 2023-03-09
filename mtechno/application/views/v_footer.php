7409ad26059ecb892f584d2050274bf4112f516c
Harfel12 <85319667+harfely@users.noreply.github.com>
2023-03-09 12:55:25 +0700

    update files

---
+++ mtechno/application/views/v_footer.php
@@ -0,0 +1,41 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+    <div class="container">
+        <div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+            <h3>About Us</h3>
+            <p>HL merupakan Portal. Pribadi untuk mendeskripsikan dan merievew pada bidang Teknik Informatika.
+            </p>
+        </div>
+        <div class="col-md-6 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+            <h3>Our Services</h3>
+            <ul class="float">
+                <li><a href="<?php echo base_url().'portfolio'?>">Web Design</a></li>
+                <li><a href="<?php echo base_url().'portfolio'?>">IT Consulting</a></li>
+                <li><a href="<?php echo base_url().'portfolio'?>">Software & Program</a></li>
+            </ul>
+            <ul class="float">
+                <li><a href="<?php echo base_url().'portfolio'?>">Hardware & Nerworking</a></li>
+                <li><a href="<?php echo base_url().'portfolio'?>">Online Marketing</a></li>
+            </ul>Gra
+
+        </div>
+
+        <div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+            <h3>Follow Us</h3>
+            <ul class="fh5co-social">
+                <li><a href="#"><i class="icon-twitter"></i></a></li>
+                <li><a href="#"><i class="icon-facebook"></i></a></li>
+                <li><a href="#"><i class="icon-google-plus"></i></a></li>
+                <li><a href="#"><i class="icon-instagram"></i></a></li>
+            </ul>
+        </div>
+
+
+        <div class="col-md-12 fh5co-copyright text-center">
+            <p>&copy; 2023 by <a href="https://github.com/harfely" target="_blank">Harfely Leipary</a>. All
+                Rights
+                Reserved.</p>
+        </div>
+
+    </div>
+</footer>
